d9f077f47e2c3ae2f53bc77e934852c693d3f2ac
louis gueye <louis.gueye@gmail.com>
2023-09-07 11:30:18 +0200

    feat: reduce the schedule delay to 20 seconds

--- create-scheduled-task.sh
+++ create-scheduled-task.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 UUID=$(uuid)
-TRIGGER_AT=$(date -u --date="+1 min" +'%Y-%m-%dT%H:%M:%S.000%:z')
+TRIGGER_AT=$(date -u --date="+20 sec" +'%Y-%m-%dT%H:%M:%S.000%:z')
 
 TASK="{\"status\":\"SUBMITTED\", \"triggerLocation\":\"http://reverse-proxy/api/v1/trigger-me\", \"label\":\"label for $UUID\", \"triggerAt\":\"$TRIGGER_AT\"}"
 echo "$TASK"
